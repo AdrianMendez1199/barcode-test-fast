caf5048c498e2b746505ba4123e9b45737635367
adrian <mendezadrian149@gmail.com>
2020-10-01 10:48:22 -0400

    wokring on enviroment camera

--- src/ScannerBarCode.js
+++ src/ScannerBarCode.js
@@ -7,35 +7,35 @@ const Scanner = () => {
 
     const divScanner = useRef(null);
     const [code, setCode] = useState('not found');
+    const [start, setStart] = useState(false);
 
     useEffect(() => {
-
-        console.log('ZXing code reader initialized2')
-        codeReader.getVideoInputDevices()
-            .then((videoInputDevices) => {
-                const selectedDeviceId = videoInputDevices[0].deviceId
-
-                codeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
-                    if (result) {
-                        setCode(result.text)
-                        console.log('result', result)
-                    }
-                }).catch((err) => {
-                    console.error(err)
-                })
-            })
-            .catch((err) => {
+        const startScanner = () => {
+            codeReader.decodeOnceFromVideoDevice(undefined, 'video').then((result) => {
+                if (result) {
+                    setCode(result.text)
+                    console.log('result', result)
+                }
+            }).catch((err) => {
                 console.error(err)
             })
-    })
+        }
+
+        startScanner()
+    }, [codeReader, start])
 
     const handleReset = () => {
         codeReader.reset();
+    }
+
+    const handleStart = () => {
+        setStart(true)
     }
     return (
         <div id="scanner-container" ref={divScanner}>
             <video id="video" className="dbrScanner-video" playsInline />
             <button onClick={handleReset}>Reset</button>
+            <button onClick={handleStart}>Start</button>
             <div>Code: {code}</div>
         </div>
     )
